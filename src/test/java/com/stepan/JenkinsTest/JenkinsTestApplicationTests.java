353913bed208d56c79f156c5a3d792bd3d60f5ef
stepan <stepan@stepans-MacBook-Air.local>
2021-09-30 12:19:43 -0600

    initialized, added jenkinsfile

---
+++ src/test/java/com/stepan/JenkinsTest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepan.JenkinsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
